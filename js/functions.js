de9f69444807499989e9fa417f870db003d276cd
zemljank <Zemljank@yandex.ru>
2026-01-29 17:28:27 +0300

    отформатировал

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,26 @@
 function lineLength(line, number) {
-return line.length <= number;
+  return line.length <= number;
 }
 
 function lineSimile(line) {
-line = line.toLowerCase();
-line = line.replaceAll(" ", "");
+  line = line.toLowerCase();
+  line = line.replaceAll(" ", "");
 
-let newStroke = "";
-for (let check = line.length - 1; check >= 0; check--) {
-  newStroke += line[check];
-}
+  let newStroke = "";
+  for (let check = line.length - 1; check >= 0; check--) {
+    newStroke += line[check];
+  }
   return line === newStroke;
-
 }
 
 function strokeSimile(stroke) {
-   stroke = stroke.toString();
-   let newStroke = "";
+  stroke = stroke.toString();
+  let newStroke = "";
 
-for (let check = 0; check < stroke.length; check++) {
-
-if (!Number.isNaN(parseInt(stroke[check]))) {
-    newStroke = newStroke + stroke[check];
-}
-
-}
-return parseInt(newStroke);
-
+  for (let check = 0; check < stroke.length; check++) {
+    if (!Number.isNaN(parseInt(stroke[check]))) {
+      newStroke = newStroke + stroke[check];
+    }
+  }
+  return parseInt(newStroke);
 }

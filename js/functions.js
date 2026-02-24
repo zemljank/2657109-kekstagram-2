56b843b01175425a93bd35603c0cdf37fa519e8c
Алексей Землянко <132543078+zemljank@users.noreply.github.com>
2026-02-24 17:16:29 +0300

    Module5 task2 (#3)
    * 4.16. Больше деталей
    
    * доработано

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,23 @@ const getRandomArrayElement = (elements) =>
   elements[getRandomInteger(0, elements.length - 1)];
 
 export { getRandomInteger, getRandomArrayElement };
+
+function getTimeInMinutes(time) {
+  const workStart = time.split(":");
+  const hours = Number(workStart[0]);
+  const minutes = Number(workStart[1]);
+  const workStartInMinutes = hours * 60 + minutes;
+  return workStartInMinutes;
+}
+
+function isMeetingInWorkTime(workStart, workEnd, meetingStart, duration) {
+  const workStartMinutes = getTimeInMinutes(workStart);
+  const workEndMinutes = getTimeInMinutes(workEnd);
+  const meetingStartMinutes = getTimeInMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + duration;
+
+  return (
+    meetingStartMinutes >= workStartMinutes &&
+    meetingEndMinutes <= workEndMinutes
+  );
+}
